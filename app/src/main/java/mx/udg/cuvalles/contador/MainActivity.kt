469b9bc24a9051fb1eb333c231f9d0a4d5c4733f
Luis Chavez <albertoluis1920@hotmail.com>
2020-05-29 15:12:36 -0500

    Initial commit

---
+++ app/src/main/java/mx/udg/cuvalles/contador/MainActivity.kt
@@ -0,0 +1,31 @@
+package mx.udg.cuvalles.contador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var contador = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        etiquetaMostrar.text = "$contador"
+
+        btnIncrementar.setOnClickListener(this)
+        btnDecrementar.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v!!.id == R.id.btnIncrementar) {
+            contador++
+        } else if (v!!.id == R.id.btnDecrementar) {
+            contador--
+        }
+        etiquetaMostrar.text = "$contador"
+    }
+
+}
